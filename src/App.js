f708bdc275e04193b1ac3a86ae115b5f1c25404f
Pd Rajan <itspdrajan@gmail.com>
2023-08-16 02:48:23 +0530

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import Content from './Content';
 import Footer from './Footer';
@@ -7,57 +7,60 @@ import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
 
 function App() {
-  
-  const [items,setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')))
 
-const [newItem,setNewItem] = useState('')
-const [search,setSearch] = useState('')
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')) || [])
 
-const setAndSaveItems = (newItem) => {
-  setItems(newItem)
-  localStorage.setItem('shoppingList',JSON.stringify(newItem))
-}
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
 
-const handleCheck = (id)=>{
-    const listItem = items.map((item)=> item.id ===id ? {...item,checked:!item.checked} : item);
-    setAndSaveItems(listItem)
-}
+  useEffect(()=>{
 
-const handleDelete = (id) => {
-    const listItem = items.filter((item)=> item.id !== id)
-    setAndSaveItems(listItem)
-}
+    localStorage.setItem('shoppingList', JSON.stringify(items))
 
-const addItem = (item) =>{
-  const id = items.length ? items[items.length - 1].id + 1 : 1;
-  const myNewItem = {id, checked:false , item}
-
-  const listItem = [...items,myNewItem]
-  setAndSaveItems(listItem)
-}
-
-const handleSubmit = (e) => {
-  e.preventDefault()
-
-  if(!newItem) return
-  console.log(newItem)
-  
-  addItem(newItem)
-  setNewItem('')
-}
+  },[items])
 
   
+
+  const handleCheck = (id) => {
+    const listItem = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItem)
+  }
+
+  const handleDelete = (id) => {
+    const listItem = items.filter((item) => item.id !== id)
+    setItems(listItem)
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item }
+
+    const listItem = [...items, myNewItem]
+    setItems(listItem)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!newItem) return
+    console.log(newItem)
+
+    addItem(newItem)
+    setNewItem('')
+  }
+
+
   return (
     <div className="App">
-      <Header title='Cross List'/>
-      <AddItem 
+      <Header title='Cross List' />
+      <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit} />
-      <SearchItem 
+      <SearchItem
         search={search}
-        setSearch={setSearch}/>
-      <Content 
+        setSearch={setSearch} />
+      <Content
         items={items.filter(item => ((item.item).toLowerCase().includes(search.toLowerCase())))}
         handleCheck={handleCheck}
         handleDelete={handleDelete} />
